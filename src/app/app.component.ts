40d9d30e6c81fce9419b75f48b933bede8b6e2c2
hrshl13 <harshal26092002@gmail.com>
2022-08-20 22:22:20 +0530

    Pushing project from local to remote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ListFormat } from 'typescript';
 
+interface BoredApi{
+  accessibility:number,
+  activity:string, 
+  key:string,
+  link:string,
+  participants:number,
+  type:string,
+  price:number
+}
+interface ListofData{
+  memes:Array<MemeApi>
+}
+interface MemeApi{
+  url:string, 
+  name:string,
+  width:number,
+  height:number
+}
+interface response{
+  data:ListofData
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +30,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Call-APIs';
+  activity:string= "";
+  url:string="";
+  MAX_MEME_COUNT=99;
+  index:number=0;
+  width=500;
+  ratio:number=0;
+  memeApi:MemeApi | undefined;
+  constructor(private http:HttpClient){}
+  bored = ()=>{
+    this.http.get<BoredApi>("http://www.boredapi.com/api/activity/").subscribe((data)=>{
+      this.activity = data.activity;
+    })
+  }
+
+  memed = ()=>{
+    this.http.get<response>("https://api.imgflip.com/get_memes").subscribe((res)=>{
+      this.index = Math.floor(Math.random() * (this.MAX_MEME_COUNT + 1));
+      this.memeApi= res.data.memes[this.index];
+      console.log(this.memeApi);
+       this.ratio = this.width/this.memeApi.width;
+       this.memeApi.height = this.memeApi.height*this.ratio;
+      ;})
+  }
 }
